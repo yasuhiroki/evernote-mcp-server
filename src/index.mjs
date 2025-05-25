88ebcc67a518d13698323a7a3771efe3b0c9ca7d
yasuhiroki <yasuhiroki.duck@gmail.com>
2025-05-25 15:49:11 +0900

    Add shebang

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
